587b21c342885335ab744c4f270e5d94eda6b877
Dmitry <sk1py@bk.ru>
2019-06-20 02:26:24 +0300

    first

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+	 <link rel="stylesheet" href="login.css">
 	<title></title>
 	<meta charset="windows 1251">
 </head>
 <body>
-	<form method='post' action='page1.php'>
-			<!--инпут в который вводим группу -->
-    	<input type='input' name='group' placeholder="группа"> 
-    		<!--инпут в который вводим предмет -->
-     	<input type='input' name='discipline' placeholder="предмет"> 
-    <input type='submit'>
-    <p>proverka svyazids</p>
-</form>
+	<div class="gg">
+        
+        <div class="gg1">
+            <img src="27.png" style="width: 200px;" class="icon">
+            <form  action="page1.php" method="post" class="form">
+                <input type='input'  name='login' class="field" placeholder="login"><br>
+                <input type='input' name='pass' placeholder="pass" class="field" ><br>
+                <input type="submit" value="" class="btn">
+            </form>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
